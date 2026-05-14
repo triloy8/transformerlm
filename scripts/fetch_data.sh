bbc0c6a39ff664e4b1abe3938dc4c84401e7f457
triloy8 <196841245+triloy8@users.noreply.github.com>
2026-05-14 18:49:15 +0200

    exp: 2k48 ctx and 4k vocab; ar

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -24,9 +24,9 @@ download() {
     mv "${tmp}" "${dest}"
 }
 
-download "https://huggingface.co/trixyL/transformerlm-diff/resolve/main/merges_simplestories_4k.txt"
-download "https://huggingface.co/trixyL/transformerlm-diff/resolve/main/vocab_simplestories_4k.json"
-download "https://huggingface.co/trixyL/transformerlm-diff/resolve/main/special_tokens_simplestories_4k.json"
+download "https://huggingface.co/trixyL/transformerlm-ar-4k-simplestories/resolve/main/merges_simplestories_4k.txt"
+download "https://huggingface.co/trixyL/transformerlm-ar-4k-simplestories/resolve/main/vocab_simplestories_4k.json"
+download "https://huggingface.co/trixyL/transformerlm-ar-4k-simplestories/resolve/main/special_tokens_simplestories_4k.json"
 download "https://huggingface.co/datasets/trixyL/simplestories-4k-megatron/resolve/main/simplestories_train_text_document.bin"
 download "https://huggingface.co/datasets/trixyL/simplestories-4k-megatron/resolve/main/simplestories_train_text_document.idx"
 download "https://huggingface.co/datasets/trixyL/simplestories-4k-megatron/resolve/main/simplestories_test_text_document.bin"
